132ce7d8267662057d679974be2c4d896ba8bd4d
Lee Reynolds <leebreynolds@protonmail.com>
2025-12-28 19:51:59 -0500

    emotion score

--- emotion_detection.py
+++ emotion_detection.py
@@ -15,4 +15,24 @@ def emotion_detector(text_to_analyse):
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
 
-    return response.text)
+    # Parse the response from the API
+    formatted_output = json.loads(response.text)
+
+    # create empty dictionary that will hold our values
+    response_dict = {}
+
+    # Get the emotion attribute scores
+    response_dict['anger'] = formatted_output['emotionPredictions'][0]['emotion']['anger']
+    response_dict['disgust'] = formatted_output['emotionPredictions'][0]['emotion']['disgust']
+    response_dict['fear'] = formatted_output['emotionPredictions'][0]['emotion']['fear']
+    response_dict['joy'] = formatted_output['emotionPredictions'][0]['emotion']['joy']
+    response_dict['sadness'] = formatted_output['emotionPredictions'][0]['emotion']['sadness']
+
+    # figure out which sentiment has the greatest value
+    response_dict['dominant_emotion'] = max(response_dict, key=response_dict.get)
+
+    # Returnt he dictionary
+    return response_dict
+
+
+    
